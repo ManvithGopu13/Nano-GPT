356092f92e8fa2bd1a9724185efd3f22a871eddf
ManvithGopu13 <manvithgopu1394@gmail.com>
2025-02-11 22:48:04 +0530

    Initial commit

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -2,7 +2,7 @@
 # good for debugging and playing on macbooks and such
 
 out_dir = 'out-shakespeare-char'
-eval_interval = 250 # keep frequent because we'll overfit
+eval_interval = 150 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10 # don't print too too often
 
@@ -25,13 +25,15 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+# max_iters = 5000
+# lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 500
+lr_decay_iters = 500 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100 # not super necessary potentially
 
 # on macbook also add
-# device = 'cpu'  # run on cpu only
-# compile = False # do not torch compile the model
+device = 'cpu'  # run on cpu only
+compile = False # do not torch compile the model
